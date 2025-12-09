6c4871b9acd2392b3de0c8daea4686b50b2fa88c
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2025-12-09 18:16:55 +0900

    added react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
     devtools(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    viteReact(),
     tailwindcss(),
   ],
   resolve: {
